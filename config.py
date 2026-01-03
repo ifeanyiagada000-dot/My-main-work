d1a132843886bc9d9bfddfbb01981b9a72397258
ifeanyiagada000-dot <ifeanyiagada000@gmail.com>
2026-01-03 07:42:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,14 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+# Get the raw value from the environment
+temp_fsub = os.environ.get("FORCE_SUB_CHANNEL", "")
+
+# Check if it looks like a number (ID) or text (Username)
+if temp_fsub.lstrip("-").isdigit():
+    FORCE_SUB_CHANNEL = int(temp_fsub)  # It's an ID (e.g., -100123...)
+else:
+    FORCE_SUB_CHANNEL = temp_fsub       # It's a Username (e.g., @MaxCinema)
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
